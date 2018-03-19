c5b27ff59c4cf3f95a3d8a169f19bb4638c934de
Ray Tsang <2fayfays@gmail.com>
2018-03-19 08:48:11 -0700

    https://github.com/bakmei/wanchai-parser/issues/12

--- src/main/java/com/dotdashline/tools/cliparser/ErrorCode.java
+++ src/main/java/com/dotdashline/tools/cliparser/ErrorCode.java
@@ -4,22 +4,32 @@
  */
 package com.dotdashline.tools.cliparser;
 
+/**
+ * A collection of parser error codes.
+ * 
+ * @author Raymond Tsang
+ * @author Steven Liang
+ *
+ * @since 0.1
+ */
 public enum ErrorCode {
-    INVALID_COMMAND(1, "Invalid command."), // The input command was invalid, probably undefined.
-    INVALID_OPTION(2, "Invalid option."),   // The input option was invalid, probably undefined.
-    INVALID_PARAM(3, "Invalid parameter"),  // The input parameter was invalid.
-    NO_INPUT(4, "No user input."),          // No user input.
-    NOT_AVAILABLE(999, "N/A");
+   INVALID_COMMAND(1, "Invalid command."), // The input command was invalid,
+                                           // probably undefined.
+   INVALID_OPTION(2, "Invalid option."), // The input option was invalid,
+                                         // probably undefined.
+   INVALID_PARAM(3, "Invalid parameter"), // The input parameter was invalid.
+   NO_INPUT(4, "No user input."), // No user input.
+   NOT_AVAILABLE(999, "N/A");
 
-    private final int levelCode;
-    private final String message;
+   private final int levelCode;
+   private final String message;
 
-    private ErrorCode(int levelCode, String msg) {
-        this.levelCode = levelCode;
-        this.message = msg;
-    }
+   private ErrorCode(int levelCode, String msg) {
+      this.levelCode = levelCode;
+      this.message = msg;
+   }
 
-    public String getMessage() {
-        return message;
-    }
+   public String getMessage() {
+      return message;
+   }
 }
--- src/main/java/com/dotdashline/tools/cliparser/utils/ReflectionUtil.java
+++ src/main/java/com/dotdashline/tools/cliparser/utils/ReflectionUtil.java
@@ -9,6 +9,8 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -26,189 +28,213 @@ import com.dotdashline.tools.cliparser.CLIParserException;
 
 public class ReflectionUtil {
 
-    public static final String newLine = System.getProperty("line.separator");
+   public static final String newLine = System.getProperty("line.separator");
 
-    public static Object createObject(Class<?> aClass, Object obj) throws NoSuchMethodException, SecurityException,
-            InstantiationException, IllegalAccessException, InvocationTargetException {
-        @SuppressWarnings("rawtypes")
-        Constructor c = aClass.getConstructor(obj.getClass());
-        return c.newInstance(obj);
-    }
+   public static Object createObject(Class<?> aClass, Object obj) throws NoSuchMethodException, SecurityException,
+         InstantiationException, IllegalAccessException, InvocationTargetException {
+      @SuppressWarnings("rawtypes")
+      Constructor c = aClass.getConstructor(obj.getClass());
+      return c.newInstance(obj);
+   }
 
-    /**
-     * Returns a collection of fields which are annotated by the given
-     * annotation.
-     * 
-     * @param objClass
-     * @param annoClass
-     * @return
-     */
-    public static List<Field> getAnnotatedFields(Class<?> objClass, Class<? extends Annotation> annotation) {
-        return Arrays.asList(objClass.getDeclaredFields()).stream().filter(x -> x.isAnnotationPresent(annotation))
-                .collect(Collectors.toList());
-    }
+   /**
+    * Returns a collection of fields which are annotated by the given
+    * annotation.
+    * 
+    * @param objClass
+    * @param annoClass
+    * @return
+    */
+   public static List<Field> getAnnotatedFields(Class<?> objClass, Class<? extends Annotation> annotation) {
+      return Arrays.asList(objClass.getDeclaredFields()).stream().filter(x -> x.isAnnotationPresent(annotation))
+            .collect(Collectors.toList());
+   }
 
-    /**
-     * Set the given value to a private field.
-     *
-     * @param obj
-     * @param field
-     * @param value
-     * @throws IllegalArgumentException
-     * @throws IllegalAccessException
-     * @throws CLIParserException
-     */
-    @SuppressWarnings("rawtypes")
-    public static void setValueToField(Object obj, Field field, Object value)
-            throws IllegalAccessException, CLIParserException {
-        boolean isAccessible = field.isAccessible();
-        if (!isAccessible) {
+   /**
+    * Set the given value to a private field.
+    *
+    * @param obj
+    * @param field
+    * @param value
+    * @throws IllegalArgumentException
+    * @throws IllegalAccessException
+    * @throws CLIParserException
+    */
+   @SuppressWarnings("rawtypes")
+   public static void setValueToField(Object obj, Field field, Object value) throws CLIParserException {
+      boolean isAccessible = field.isAccessible();
+
+      try {
+         if (!isAccessible) {
             field.setAccessible(true);
-        }
-        try {
+         }
+         try {
             field.set(obj, value);
-        } catch (IllegalArgumentException e) {
+         } catch (IllegalAccessException | IllegalArgumentException e) {
             // try to call the constructor with java.langString
             Constructor c;
             try {
-                if (field.getType().isPrimitive()) {
-                    setPrimitiveValueToField(obj, field, value);
-                } else {
-                    c = field.getType().getConstructor(String.class);
-                    field.set(obj, c.newInstance(value));
-                }
-            } catch (IllegalArgumentException | InstantiationException | InvocationTargetException
-                    | NoSuchMethodException | SecurityException e1) {
-                throw new CLIParserException(String.format(
+               if (field.getType().isPrimitive()) {
+                  setPrimitiveValueToField(obj, field, value);
+               } else {
+                  c = field.getType().getConstructor(String.class);
+                  field.set(obj, c.newInstance(value));
+               }
+            } catch (IllegalAccessException | IllegalArgumentException | InstantiationException
+                  | InvocationTargetException | NoSuchMethodException | SecurityException e1) {
+               // try last time, see if there is a static method for
+               // instantiating
+               // the object
+               Method m = ReflectionUtil.findMethod(obj.getClass(), true, field.getType(), value.getClass());
+               if (m != null) {
+                  try {
+                     field.set(obj, m.invoke(null, value));
+                  } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e2) {
+                     throw new CLIParserException("Failed to use static method for instantiating the field value.");
+                  }
+               } else {
+                  throw new CLIParserException(String.format(
                         "The token doesn't match the type of the option/param field, or the custom type doesn't provide a java.lang.String constructor: %s %s",
                         field.getName(), value), e1);
+               }
             }
-        }
-        // reset the field to private if it was private
-        if (!isAccessible) {
+         }
+      } catch (Exception e) {
+         // this try/catch is for the need of finally block
+         throw e;
+      } finally {
+         // reset the field to private if it was private
+         if (!isAccessible) {
             field.setAccessible(false);
-        }
-    }
+         }
+      }
+   }
 
-    private static void setPrimitiveValueToField(Object obj, Field field, Object value)
-            throws IllegalArgumentException, IllegalAccessException {
+   private static void setPrimitiveValueToField(Object obj, Field field, Object value)
+         throws IllegalArgumentException, IllegalAccessException {
 
-        if (field.getType().equals(boolean.class)) {
-            field.setBoolean(obj, Boolean.parseBoolean(value.toString()));
+      if (field.getType().equals(boolean.class)) {
+         field.setBoolean(obj, Boolean.parseBoolean(value.toString()));
 
-        } else if (field.getType().equals(int.class)) {
-            field.setInt(obj, Integer.parseInt(value.toString()));
+      } else if (field.getType().equals(int.class)) {
+         field.setInt(obj, Integer.parseInt(value.toString()));
 
-        } else if (field.getType().equals(long.class)) {
-            field.setLong(obj, Long.parseLong(value.toString()));
+      } else if (field.getType().equals(long.class)) {
+         field.setLong(obj, Long.parseLong(value.toString()));
 
-        } else if (field.getType().equals(float.class)) {
-            field.setFloat(obj, Float.parseFloat(value.toString()));
+      } else if (field.getType().equals(float.class)) {
+         field.setFloat(obj, Float.parseFloat(value.toString()));
 
-        } else if (field.getType().equals(double.class)) {
-            field.setDouble(obj, Double.parseDouble(value.toString()));
+      } else if (field.getType().equals(double.class)) {
+         field.setDouble(obj, Double.parseDouble(value.toString()));
 
-        } else if (field.getType().equals(byte.class)) {
-            field.setByte(obj, Byte.parseByte(value.toString()));
+      } else if (field.getType().equals(byte.class)) {
+         field.setByte(obj, Byte.parseByte(value.toString()));
 
-        } else if (field.getType().equals(short.class)) {
-            field.setShort(obj, Short.parseShort(value.toString()));
+      } else if (field.getType().equals(short.class)) {
+         field.setShort(obj, Short.parseShort(value.toString()));
 
-        } else if (field.getType().equals(char.class)) {
-            field.setChar(obj, value.toString().charAt(0));
-        }
-    }
+      } else if (field.getType().equals(char.class)) {
+         field.setChar(obj, value.toString().charAt(0));
+      }
+   }
 
-    public static Object createCommandObject(Class<?> clazz) throws InstantiationException, IllegalAccessException {
-        if (clazz == null) {
-            throw new IllegalArgumentException("The input class is null.");
-        }
-        return clazz.newInstance();
-    }
+   public static Object createCommandObject(Class<?> clazz) throws InstantiationException, IllegalAccessException {
+      if (clazz == null) {
+         throw new IllegalArgumentException("The input class is null.");
+      }
+      return clazz.newInstance();
+   }
 
-    /**
-     * Update the rest of the tokens in the queue to the array type field.
-     * 
-     * @param cmdObj
-     * @param field
-     * @param tokenQueue
-     *            throws CLIParserException
-     */
-    public static void setValuesToArrayField(Object cmdObj, Field field, List<String> tokens)
-            throws CLIParserException {
-        // get the type of the array
-        Class<?> arrayType = field.getType().getComponentType();
+   /**
+    * Update the rest of the tokens in the queue to the array type field.
+    * 
+    * @param cmdObj
+    * @param field
+    * @param tokenQueue
+    *           throws CLIParserException
+    */
+   public static void setValuesToArrayField(Object cmdObj, Field field, List<String> tokens) throws CLIParserException {
+      // get the type of the array
+      Class<?> arrayType = field.getType().getComponentType();
 
-        // create an instance of the array
-        Object[] paramArray = (Object[]) Array.newInstance(arrayType, tokens.size());
+      // create an instance of the array
+      Object[] paramArray = (Object[]) Array.newInstance(arrayType, tokens.size());
 
-        // update the array with he token in the queue.
-        for (int i = 0; i < paramArray.length; i++) {
-            try {
-                paramArray[i] = ReflectionUtil.createObject(arrayType, tokens.get(i));
-            } catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException
-                    | InvocationTargetException e) {
-                throw new CLIParserException("Failed to set array type values into parameter field.", e);
-            }
-        }
-
-        // finally set the array to the field
-        try {
-            ReflectionUtil.setValueToField(cmdObj, field, paramArray);
-        } catch (IllegalAccessException e) {
+      // update the array with he token in the queue.
+      for (int i = 0; i < paramArray.length; i++) {
+         try {
+            paramArray[i] = ReflectionUtil.createObject(arrayType, tokens.get(i));
+         } catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException
+               | InvocationTargetException e) {
             throw new CLIParserException("Failed to set array type values into parameter field.", e);
-        }
-    }
+         }
+      }
 
-    public static String toString(Object obj) {
-        return toString(obj, 0);
-    }
+      // finally set the array to the field
+      ReflectionUtil.setValueToField(cmdObj, field, paramArray);
+   }
 
-    private static String toString(Object obj, int indent) {
+   public static Method findMethod(Class<?> clazz, boolean isStatic, Class<?> returnType, Class<?>... paramTypes) {
+      Method[] methods = clazz.getDeclaredMethods();
+      for (Method m : methods) {
+         if (m.getReturnType().equals(returnType) && Modifier.isStatic(m.getModifiers())
+               && Arrays.asList(paramTypes).equals(Arrays.asList(m.getParameterTypes()))) {
+            return m;
+         }
+      }
+      return null;
+   }
 
-        if (obj == null) {
-            return "<NULL>";
-        }
+   public static String toString(Object obj) {
+      return toString(obj, 0);
+   }
 
-        StringBuilder ret = new StringBuilder();
+   private static String toString(Object obj, int indent) {
 
-        ret.append(getIndentString(indent)).append("[ ").append(obj.getClass().getSimpleName()).append(" ]")
-                .append(newLine);
+      if (obj == null) {
+         return "<NULL>";
+      }
 
-        final int childIndent = indent + 1;
-        Arrays.asList(obj.getClass().getDeclaredFields()).stream().forEach(x -> {
-            ret.append(getIndentString(childIndent)).append(x.getName()).append(" (")
-                    .append(x.getType().getSimpleName()).append(") -> ");
-            Object childObj = null;
-            boolean isAccessible = x.isAccessible();
-            if (!isAccessible) {
-                x.setAccessible(true);
-            }
-            try {
-                childObj = x.get(obj);
-            } catch (IllegalArgumentException | IllegalAccessException e) {
-                ret.append(e.getMessage());
-            }
+      StringBuilder ret = new StringBuilder();
 
-            // restore the value
-            if (!isAccessible) {
-                x.setAccessible(false);
-            }
+      ret.append(getIndentString(indent)).append("[ ").append(obj.getClass().getSimpleName()).append(" ]")
+            .append(newLine);
 
-            if (childObj == null || childObj.getClass() == null || childObj.getClass().getPackage() == null) {
-                ret.append("<NULL>");
-            } else if (childObj.getClass().getPackage().getName().startsWith("com.dotdashline")) {
-                ret.append(toString(childObj, childIndent + 1));
-            } else {
-                ret.append(childObj.toString());
-            }
-            ret.append(newLine);
-        });
+      final int childIndent = indent + 1;
+      Arrays.asList(obj.getClass().getDeclaredFields()).stream().forEach(x -> {
+         ret.append(getIndentString(childIndent)).append(x.getName()).append(" (").append(x.getType().getSimpleName())
+               .append(") -> ");
+         Object childObj = null;
+         boolean isAccessible = x.isAccessible();
+         if (!isAccessible) {
+            x.setAccessible(true);
+         }
+         try {
+            childObj = x.get(obj);
+         } catch (IllegalArgumentException | IllegalAccessException e) {
+            ret.append(e.getMessage());
+         }
 
-        return ret.toString();
-    }
+         // restore the value
+         if (!isAccessible) {
+            x.setAccessible(false);
+         }
 
-    private static Object getIndentString(int indent) {
-        return String.join("", Collections.nCopies(indent * 3, " "));
-    }
+         if (childObj == null || childObj.getClass() == null || childObj.getClass().getPackage() == null) {
+            ret.append("<NULL>");
+         } else if (childObj.getClass().getPackage().getName().startsWith("com.dotdashline")) {
+            ret.append(toString(childObj, childIndent + 1));
+         } else {
+            ret.append(childObj.toString());
+         }
+         ret.append(newLine);
+      });
+
+      return ret.toString();
+   }
+
+   private static Object getIndentString(int indent) {
+      return String.join("", Collections.nCopies(indent * 3, " "));
+   }
 }
